c8057489fea1f16be54cb6dbdf86c4f495b3bb22
hackonn <hackonn@Hackonn.Hackonn>
2022-10-12 11:48:19 -0400

    vlauchTast

---
+++ Desktop38/welcome.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VlaunchU</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="css1.css">
+</head>
+
+<body>
+    <form class="myform">
+        <div class="container">
+            <div id="image-logo ">
+                <img src="image 1.png" alt="" style="margin-top:75px; width: 404px;height:97px;">
+            </div>
+            <div class="myform">
+                <h6 class="title">Project Details</h6>
+                <div class="box-container">
+                    <div id="container1">
+                        <div class="form-contro">
+                            <p class="form-data">Job Title(*) </p>
+                            <input type="text" style="margin-top: -20px;margin-left:-5px">
+                            <p class="form-data">No. Of Openings(*) </p>
+                            <input type="text" style="margin-top: -20px;margin-left:-5px">
+                            <p class="form-data">Job Location(Please Select the local from Dropdown)(*) </p>
+                            <input type="text" style="margin-top: -20px;margin-left:-5px">
+                            <p class="form-data"
+                                style="Display:flex;justify-content: flex-end;margin-right: 238px;margin-top: -74px">
+                            </p>
+                            <input type="text"
+                                style="margin-top: 44px;margin-left:274px; Display:flex;justify-content: flex-end;">
+                        </div>
+
+                        <h6 class="title" style="font-size:20px;margin-top:30px;color:black ">Additional Job Detais</h6>
+                        <div id="box-container2">
+                            <div class="form-contro">
+                                <p class="form-data">Job Location(Please Select the local from Dropdown)(*) </p>
+                                <input type="text" style="margin-top: -20px;margin-left:-5px"> to
+                                <p class="form-data"
+                                    style="Display:flex;justify-content: flex-end;margin-right: 238px;margin-top: -74px">
+                                </p>
+                                <input type="text"
+                                    style="margin-top: 44px;margin-left:290px; Display:flex;justify-content: flex-end;">
+                                <p class="form-data" style="margin-top:50px">No. Of Openings(*) </p>
+                                <input type="text" style="margin-top: -57px;margin-left:-5px">
+                                <p style="margin-top:20px; margi-left:10px;">Remaining Character:350</p>
+                            </div>
+                            <div id="container3">
+                                <div class="form-contro">
+                                    <p style="color:black;font-size:15px;margin-left:10px;margin-bottom: -1px;">Job
+                                        Timings:
+                                        9:30 am - 6:30pm | Monday to S...</p>
+                                    <p style="color:black;font-size:15px;margin-left:10px">Interview Details: 11:00 am -
+                                        4:00pm | Monday to ...</p>
+                                </div>
+                                <h6 class="title" style="font-size:20px;margin-top:-145px  ;color:black ">About Your
+                                    Company
+                                </h6>
+                                <div id="container-4">
+                                    <div class="form-contro">
+                                        <p class="form-data">Company Name* </p>
+                                        <input type="text" style="margin-top: -20px;margin-left:-5px">
+                                        <p class="form-data"
+                                            style="Display:flex;justify-content: flex-end;margin-right: 165px;margin-top: -74px">
+                                            Contact Person Number*</p>
+                                        <input type="text"
+                                            style="margin-top: px;margin-left:274px; Display:flex;justify-content: flex-end;">
+                                        <p class="form-data">Phone Number* </p>
+                                        <input type="text" style="margin-top: -20px;margin-left:-5px">
+                                        <p class="form-data"
+                                            style="Display:flex;justify-content: flex-end;margin-right: 238px;margin-top: -74px">
+                                            Email-ID*</p>
+                                        <input type="text"
+                                            style="margin-top: 0px;margin-left:274px;Display:flex;justify-content: flex-end;">
+                                        <p class="form-data" style="margin-top:5px">Job Address* </p>
+                                        <textarea name="" id="" cols="35" rows="2"></textarea>
+                                    </div>
+                                    <button class="btn"
+                                        style="background-color:white;color:black;margin-top:59px;margin-left:228px;width: 138px;height: 38px;margin-bottom: 34px;">Submit</button>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </form>
+</body>
+
+</html>
---
+++ mackbook 14 4-2/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</head>
+  <body>
+    <form action="#">
+    <div class="container">
+        <img src="image3.png" alt="image">
+        <p type="list" style="margin-top:20px;color:black;font-size:23px;font-weight:700px">Candidates will be able to report the job postings in case of any violation of rules or sharing of unauthentic, information or in case of any abuse for the admins to take action.</p>
+    <div class="btn-group" >
+     <button class="btn btn-secondary btn-lg bg-light" style="color:black;"  type="button">
+      Go To Job Profile
+     </button>
+     <button type="button" class="btn btn-lg btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+    <span class="visually-hidden" >Toggle Dropdown</span>
+  </button>
+  <ul class="dropdown-menu">
+    <li>hello</li>
+  </ul>
+</div>  
+    </div>
+    <textarea name="" id="" cols="30" rows="5" style="margin-top:20px;margin-left: 100px;background: #D9D9D9;border-style: none;">Report</textarea>
+<button class="btn btn-success" style=" background: #319CB4;margin-left: 78px;border: 0%;border-style: none;position: absolute;border-style: none;border-radius: 0%; width: 135px; margin-top: 79px;color: black;font-size: 23px;">Submit</button>
+</form>
+
+</body>
+</html>
