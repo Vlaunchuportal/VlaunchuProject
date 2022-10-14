eebe990332d6014bcbfe3f5ab7a0af3b84ec3083
shubhamsahu4747 <115063193+shubhamsahu4747@users.noreply.github.com>
2022-10-14 15:38:02 +0530

    Add files via upload

---
+++ wallet1/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css1.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</head>
+  <body>
+  <div class="container">
+    <div class="nav-item">
+     <nav>   
+    <ion-icon name="chevron-back-outline" style="margin-top:10px"></ion-icon>
+    <a href="#" class="home-page">Home</a>
+    </nav>
+    <h1 class="title-heading">Total wallet Points</h1>
+    <div class="content">
+     <img src="../wallet1/components/image 17.png" alt="">   
+    <p id="para-heading" style="margin-left:226px;margin-top:-104px;">125 points</p>
+    <button class="submit-button" style="margin-left:41px;margin-top:157px;" >spend on subscription</button>
+   <p class="fotter-quto" style="margin-left:-71px;margin-top:257px;">Earn more points by inviting friends</p> 
+   </div>
+   </div>
+  </div>
+  </body>
+</html>
